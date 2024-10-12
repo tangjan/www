d56f92cdebe84bb8a5f7b83e50a4cfea0816d67c
Jan Tang <tangjiayan2019@gmail.com>
2024-10-12 18:24:42 +0800

    remove footer

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -91,8 +91,8 @@ export default defineConfig({
       },
     ],
 
-    footer: {
-      copyright: "Copyright © 2024 糖加盐",
-    },
+    // footer: {
+    //   copyright: "Copyright © 2024 糖加盐",
+    // },
   },
 });
